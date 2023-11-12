407c9ce89ec20bf862fed1cffb71809fd1f61f93
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-11-12 20:07:01 +0530

    updated

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@bistrobossrestaurantclu.wmsavpc.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,8 +25,8 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const menuCollection = client.db("bistroDb").collection("menu");
-    const reviewCollection = client.db("bistroDb").collection("reviews");
+    const menuCollection = client.db("bistroDB").collection("menus");
+    const reviewCollection = client.db("bistroDB").collection("reviews");
 
     app.get('/menu', async(req, res) =>{
         const result = await menuCollection.find().toArray();
