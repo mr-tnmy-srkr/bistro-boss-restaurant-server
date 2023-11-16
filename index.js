c5bdc4a6769257b08f5ba7d213f243f53263a1b4
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-11-16 20:36:42 +0530

    updated till 8:36pm

--- index.js
+++ index.js
@@ -31,6 +31,27 @@ async function run() {
     const usersCollection = client.db("bistroDB").collection("users");
 
     // users related apis
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get('/users/admin/:email', async (req, res) => {
+      const email = req.params.email;
+
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    })
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       const query = { email: user.email }
